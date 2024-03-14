9d3bf78433cb0a6513be0097e5001209ea0272bd
Faridah Salami <f.salami@outlook.com>
2024-03-14 14:53:02 +0000

    5 a day function created and working

--- development/JAVASCRIPT/script.js
+++ development/JAVASCRIPT/script.js
@@ -7,13 +7,33 @@ function calculateYearsUntilRetirement(event) {
 }
 
 function getGreetingInFrench(event) {
-    const greeting = event.target.value
+    const greeting = event.target.value;
 
     if(greeting =="Hello") {
         alert("Bonjour")
     } else if(greeting == "Goodbye") {
-        alert("Aurevoir")
+        alert("Au revoir")
     } else {
         alert (`${greeting} is not recognised`)
+    }
+}
+
+function trackFiveDaily(event) {
+    const fruit = event.target.value;
+    
+    if(fruit == 5){
+        alert("You have eaten all your fruits today")
+    } else if(fruit != 5 && fruit < 5 && fruit > 0){
+        let total = 5 - fruit;
+        alert(`You have ${total} more fruits to eat today`)
+    } else if(fruit == 0){
+        alert("You have eaten no fruits today")
+    }else if(fruit <= 8){
+        alert(`Wow! You have eaten ${fruit} fruits today! Very healthy`)
+    }else if(fruit > 8){
+        alert("Be careful not to over do it. Everything in moderation")
+    }
+    else {
+        alert("Value not recognised. Please enter how many fruits you have eaten today")
     }
 }
