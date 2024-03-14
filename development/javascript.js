d20ce938e11d8c16f32bb69db8f40f3dbf2c515c
Faridah Salami <f.salami@outlook.com>
2024-03-14 14:09:50 +0000

    js file linked

---
+++ development/javascript.js
@@ -0,0 +1,43 @@
+console.log("Hello Part-timers")
+
+const coach = "Carter";
+
+if (coach == "Carter"){
+    console.log("It's a coach!")
+}
+
+const  greeting = "hola";
+
+if (greeting == "bonjour"){
+    console.log("commet ca va?")
+}
+
+const score = 10;
+
+if(score >= 10){
+    console.log("You WON!!")
+}
+
+
+let age = 78;
+
+if (age > 65){
+    console.log("Well done, you can finally retire x")
+}
+
+const hasCovid = false;
+
+if(hasCovid){
+    console.log("Time to isolate")
+} else{
+    console.log("You can keep social distancing")
+}
+
+age = 19;
+const isBritish = true;
+
+if(age > 18 && isBritish){
+    console.log("You can vote in the next election!")
+} else{
+    console.log("Better luck next time")
+}
