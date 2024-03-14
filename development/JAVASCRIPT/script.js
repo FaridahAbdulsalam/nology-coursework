b1f6c7ab1500808796efd7973b23f001ffa6dfa4
Faridah Salami <f.salami@outlook.com>
2024-03-14 14:31:56 +0000

    second function of challnge done

--- development/JAVASCRIPT/script.js
+++ development/JAVASCRIPT/script.js
@@ -1,8 +1,19 @@
-function calculateYearsUntilRetirement(event){
+function calculateYearsUntilRetirement(event) {
     const currentAge = event.target.value;
 
-    let retireAge = 65 - currentAge
+    let retireAge = 65 - currentAge;
 
     alert(`You can retire in ${retireAge} years`)
 }
 
+function getGreetingInFrench(event) {
+    const greeting = event.target.value
+
+    if(greeting =="Hello") {
+        alert("Bonjour")
+    } else if(greeting == "Goodbye") {
+        alert("Aurevoir")
+    } else {
+        alert (`${greeting} is not recognised`)
+    }
+}
