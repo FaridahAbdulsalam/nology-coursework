304fa36f635c500358b2d5f77e3d3e28bd623ab1
Faridah Salami <f.salami@outlook.com>
2024-03-15 23:31:03 +0000

    debugging code along finished

--- development/JS-101/SUPERMARKET/script.js
+++ development/JS-101/SUPERMARKET/script.js
@@ -1,16 +1,16 @@
-const currentHedline = "News in the world today"
+let currentHedline = "News in the world today"
 const currentArticle = "    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusantium qui nihil voluptatem quaerat doloribus unde suscipit omnis quia quibusdam velit saepe, perspiciatis magnam dolorum distinctio nisi doloremque sed laborum sunt."
 
 const isFakeNews = false; 
 const amountOfWords = 957;
 
-if (headline.length > 20) {
-    headline = headline + "...";
+if (currentHedline.length > 15) {
+    currentHedline = currentHedline.substring(0, 15) + "...";
 }
 
 console.log("Nology Post 2024")
-console.log(`Headline: ${headline}`)
-console.log(article);
+console.log(`Headline: ${currentHedline}`)
+console.log(currentArticle);
 
 function getPost() {
     //Get your post in here...
