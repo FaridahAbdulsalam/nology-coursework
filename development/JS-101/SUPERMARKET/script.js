8283e857fc26c86e0aa36291c4d43b673eb1e28a
Faridah Salami <f.salami@outlook.com>
2024-03-15 23:07:38 +0000

    Code along started

---
+++ development/JS-101/SUPERMARKET/script.js
@@ -0,0 +1,17 @@
+const currentHedline = "News in the world today"
+const currentArticle = "    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusantium qui nihil voluptatem quaerat doloribus unde suscipit omnis quia quibusdam velit saepe, perspiciatis magnam dolorum distinctio nisi doloremque sed laborum sunt."
+
+const isFakeNews = false; 
+const amountOfWords = 957;
+
+if (headline.length > 20) {
+    headline = headline + "...";
+}
+
+console.log("Nology Post 2024")
+console.log(`Headline: ${headline}`)
+console.log(article);
+
+function getPost() {
+    //Get your post in here...
+}
