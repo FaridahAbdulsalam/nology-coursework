e04e4444a4fd80425a9d6db6dd0763842109dac1
Faridah Salami <f.salami@outlook.com>
2024-03-16 01:50:39 +0000

    Arrays code along done

---
+++ development/JS-101/ARRAYS/script.js
@@ -0,0 +1,21 @@
+//1. Create Array of coaches
+const coaches = ["Connor", "Carter", "Rach"]
+console.log(coaches[0]);
+
+//2. Create array of pets
+const pets = ["Laila", "Molly", "Fifi", "Lexi", "Stark", "Thor"]
+console.log(pets[4])
+
+const family = ["Dad", "Mum", "Khadeejah", "Khalid", "Khulaysa"]
+console.log(family[2]);
+console.log(family[0]);
+console.log(family[4]);
+
+const colours = ["pink", "purple", "yellow", "blue", "grey", "black", "white"]
+console.log(colours[5]);
+console.log(colours[0]);
+console.log(colours[2]);
+console.log(colours[3]);
+
+
+
