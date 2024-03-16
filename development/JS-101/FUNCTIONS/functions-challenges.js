8ab4c2c7aa1280eb75f8c5f8ba4a68608a77eaab
Faridah Salami <f.salami@outlook.com>
2024-03-16 02:21:08 +0000

    challeng 1 done

---
+++ development/JS-101/FUNCTIONS/functions-challenges.js
@@ -0,0 +1,15 @@
+//1. Write a function that takes a number and returns true if it is a positive number and false if it is a negative number.
+function numberIdentifier(num){
+    if(num >= 0){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+console.log(numberIdentifier(7));
+
+//2. Write a function that takes a number of days and converts it into an age.
+//3. Write a function that takes three numbers and returns the largest of the three numbers.
+//4. Write a function that takes an array of names and returns the last name from the array of names.
+//5. Write a function that takes an array of numbers and returns true if all of the numbers are positive. It should return false if there are one or more negative numbers in the array.
