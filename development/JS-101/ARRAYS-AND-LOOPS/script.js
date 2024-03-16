4c86a1f3c12b0b4898202c48ed890a55fdb15792
Faridah Salami <f.salami@outlook.com>
2024-03-16 02:05:39 +0000

    Array and index done

--- development/JS-101/ARRAYS-AND-LOOPS/script.js
+++ development/JS-101/ARRAYS-AND-LOOPS/script.js
@@ -1,25 +1,30 @@
 //1. Create Array of coaches
 const coaches = ["Connor", "Carter", "Rach"]
-console.log(coaches[0]);
+// console.log(coaches[0]);
 
 //2. Create array of pets
 const pets = ["Laila", "Molly", "Fifi", "Lexi", "Stark", "Thor"]
-console.log(pets[4])
+// console.log(pets[4])
 
 const family = ["Dad", "Mum", "Khadeejah", "Khalid", "Khulaysa"]
-console.log(family[2]);
-console.log(family[0]);
-console.log(family[4]);
+// console.log(family[2]);
+// console.log(family[0]);
+// console.log(family[4]);
 
 const colours = ["pink", "purple", "yellow", "blue", "grey", "black", "white"]
-console.log(colours[5]);
-console.log(colours[0]);
-console.log(colours[2]);
-console.log(colours[3]);
+// console.log(colours[5]);
+// console.log(colours[0]);
+// console.log(colours[2]);
+// console.log(colours[3]);
 
 //3. Create a smaple for loop
 
-for (let index = 0; index < 10; index++){
-    console.log("The index for this loop is " + index)
-}
+// for (let index = 0; index < 10; index++){
+//     console.log("The index for this loop is " + index)
+// }
 
+//Code ALong 
+
+for (let index = 0; index < pets.length; index++){
+    console.log(pets[index])
+}
